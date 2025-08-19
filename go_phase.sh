f2cdbce82a1f24e39c426125485b3e60df8378cd
Jean-Michel Friedt <friedtj@free.fr>
2025-08-19 20:13:41 +0200

    try to mix S1A and S1C

--- go_phase.sh
+++ go_phase.sh
@@ -1,6 +1,8 @@
 a=""
-for i in S1C*.zip; do 
-# for i in S1B*.zip; do 
+# for i in S1C*.zip; do 
+for i in S1A*.zip; do 
+# for i in `ls *zip | sort -t_ -k 5.5`; do # check with sort -t_ -k 5.5 --debug  # reorder according to date but different polarizations cannot be used for InSAR => no interferogram between S1A and S1C
+   echo $a $i
    if test -z "$a" 
       then echo "not good"
       else cat Makefile_phase.template | sed "s/NOM1/$a/g" | sed "s/NOM2/$i/g" > Makefile  
