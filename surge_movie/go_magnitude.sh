8048804440463f5e3d460703aa8f732803e0e5c4
Jean-Michel Friedt <friedtj@free.fr>
2026-05-11 12:05:14 +0200

    add DEM and PNG conversion

--- surge_movie/go_magnitude.sh
+++ surge_movie/go_magnitude.sh
@@ -1,12 +1,13 @@
 for i in `ls -d *S1*zip.data`; do
   echo $i
   nom=`echo $i | cut -c 18-25`
-  laliste=$( ls ${nom}*tif ${nom}*tif.gz )
+  laliste=$( ls ${nom}*tif ${nom}*tif.gz ${nom}.png )
   if [ -z "$laliste" ]; then 
-    gdal_translate -projwin_srs EPSG:32633 -projwin 450000 8763600 490000 8717000 -of GTiff ${i}/Int*HV*.img ${nom}_final_intensityHV.tif
-    gdal_translate -projwin_srs EPSG:32633 -projwin 450000 8763600 490000 8717000 -of GTiff ${i}/Int*VH*.img ${nom}_final_intensityVH.tif
-    gdal_translate -projwin_srs EPSG:32633 -projwin 450000 8763600 490000 8717000 -of GTiff ${i}/Int*HH*.img ${nom}_final_intensityHH.tif
-    gdal_translate -projwin_srs EPSG:32633 -projwin 450000 8763600 490000 8717000 -of GTiff ${i}/Int*VV*.img ${nom}_final_intensityVV.tif
+    gdal_translate -projwin_srs EPSG:32633 -projwin 450000 8763600 496000 8717000 -of GTiff ${i}/Int*HV*.img ${nom}_final_intensityHV.tif
+    gdal_translate -projwin_srs EPSG:32633 -projwin 450000 8763600 496000 8717000 -of GTiff ${i}/Int*VH*.img ${nom}_final_intensityVH.tif
+    gdal_translate -projwin_srs EPSG:32633 -projwin 450000 8763600 496000 8717000 -of GTiff ${i}/Int*HH*.img ${nom}_final_intensityHH.tif
+    gdal_translate -projwin_srs EPSG:32633 -projwin 450000 8763600 496000 8717000 -of GTiff ${i}/Int*VV*.img ${nom}_final_intensityVV.tif
+    gdal_translate -of PNG -ot Uint16 -scale 0 30000. 0 65535 ${nom}_final_intensityHH.tif ${nom}_final_intensityHH.png
   else 
     echo $nom $laliste "done" ;
     rm -rf S1A*${nom}*.zip
